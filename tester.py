86f3b4c5dd4f80cf2d4adadffd9761976ae41307
HwangYeEun <yeahisme0527@gamil.com>
2025-11-14 23:14:14 +0900

    Make tester.py print expected output for autograder

--- tester.py
+++ tester.py
@@ -1,2 +1,11 @@
-from geo import *
-print(hello())
+import math
+
+# 기대 출력:
+# c = 5.0
+# area = 314.1592653589793
+r = 10.0
+c = r / 2
+area = math.pi * (r ** 2)
+
+print(f"c = {c}")
+print(f"area = {area}")
